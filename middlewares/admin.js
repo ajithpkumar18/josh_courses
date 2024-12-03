32c38e6bb740aed11d90d6ef6e641d104856917c
Ajith <ajithpkumar231@gmail.com>
2024-12-03 21:01:50 +0530

    updated the error handling

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -2,13 +2,19 @@ const jwt = require("jsonwebtoken")
 
 let adminMiddleware = (req, res, next) => {
     const token = req.cookies.access_token;
-    let decodedToken = jwt.verify(token, process.env.JWT_ADMIN_PASSWORD)
-    if (decodedToken) {
-        req.userId = decodedToken.username
-        next();
+    try {
+
+        let decodedToken = jwt.verify(token, process.env.JWT_ADMIN_PASSWORD)
+        if (decodedToken) {
+            req.userId = decodedToken.username
+            next();
+        }
+        else {
+            return res.status(402).json("Expired token")
+        }
     }
-    else {
-        return res.status(402).json("Expired token")
+    catch (e) {
+        return res.status(404).json({ message: "Page not accesible for normal user" })
     }
 }
 
--- middlewares/user.js
+++ middlewares/user.js
@@ -4,17 +4,21 @@ let userMiddleware = (req, res, next) => {
     console.log("here");
 
     const token = req.cookies.access_token;
+    try {
 
-    let decodedToken = jwt.verify(token, process.env.JWT_USER_PASSWORD)
-    console.log(decodedToken);
+        let decodedToken = jwt.verify(token, process.env.JWT_USER_PASSWORD)
+        console.log(decodedToken);
 
-    if (decodedToken) {
-        req.userId = decodedToken.username
-        console.log(decodedToken.username)
-        next();
-    }
-    else {
-        return res.status(402).json("Expired token")
+        if (decodedToken) {
+            req.userId = decodedToken.username
+            console.log(decodedToken.username)
+            next();
+        }
+        else {
+            return res.status(402).json("Expired token")
+        }
+    } catch (e) {
+        res.status(404).json({ message: "You are not logged in" })
     }
 }
 
